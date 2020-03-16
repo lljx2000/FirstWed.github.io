b83a01bdf6dbe8b7584bf2d975c8df399ba574e4
ljx <1026796388@qq.com>
2020-03-16 15:12:02 +0800

    3.16

--- C8/js/topScroll.js
+++ C8/js/topScroll.js
@@ -1,12 +1,10 @@
 //控制顶部搜索栏的显示与隐藏
 window.onscroll = function() {
-    console.log("页面滑动了");
     //获取滚动条滚动的位置
     var top = document.documentElement.scrollTop;
-    console.log(top);
     //获取到顶部的div class=navigationbar
     var nav = document.getElementsByClassName('nav')[0];
-    if (top >= 100) {
+    if (top >= 150) {
         nav.style.position = "fixed";
         nav.style.top = "0";
         nav.style.height = "48px";
